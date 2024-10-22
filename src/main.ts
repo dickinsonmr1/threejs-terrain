05d821d61bebdd1357880e892e32b4100e433808
Mark Dickinson <dickinsonmr1@gmail.com>
2024-10-22 05:26:51 -0400

    creating basic Three.js scene with box

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,24 @@
 import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import * as THREE from 'three'
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+const scene = new THREE.Scene();
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material =new THREE.MeshBasicMaterial({color: 0x00ff00});
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+const mesh = new THREE.Mesh(geometry, material);
+
+scene.add(mesh);
+
+const temp = {
+  width: 1024,
+  height: 720
+};
+
+const camera = new THREE.PerspectiveCamera(75, temp.width / temp.height);
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(temp.width, temp.height);
+document.body.appendChild(renderer.domElement);
+
+camera.position.z = 4;
+renderer.render(scene, camera);
