caf5c8e4dda8cc82ca5452260a21436e9aa4f624
Mark Dickinson <dickinsonmr1@gmail.com>
2024-12-19 20:54:07 -0500

    additional build config

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ export default defineConfig({
   base: '/threejs-terrain/', // Replace 'my-threejs-app' with your GitHub repository name
   define: {
     __BUILD_DATE__: JSON.stringify(new Date().toISOString()),
-    __BUILD_NUMBER__: JSON.stringify(process.env.BUILD_NUMBER || '1'),
+    __BUILD_NUMBER__: JSON.stringify(process.env.VITE_BUILD_NUMBER  || '1'),
   },
 });
