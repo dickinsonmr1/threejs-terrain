a1443c924878bf1898c1761679efc62893cd3bec
Mark Dickinson <dickinsonmr1@gmail.com>
2026-06-02 16:36:12 -0400

    ensure different pages work when published

--- buttonEventListeners.ts
+++ buttonEventListeners.ts
@@ -1,16 +1,16 @@
 const button1 = document.getElementById("demo1Button");
 button1?.addEventListener("click", () => {
-    window.location.href = "demo1.html";
+    window.location.href = "./demo1.html";
 });
 
 const button2 = document.getElementById("demo2Button");
 button2?.addEventListener("click", () => {
-    window.location.href = "demo2.html";
+    window.location.href = "./demo2.html";
 });
 
 const button3 = document.getElementById("demo3Button");
 button3?.addEventListener("click", () => {
-    window.location.href = "demo3.html";
+    window.location.href = "./demo3.html";
 });
 
 const landingPageButton = document.getElementById("landingPageButton");
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,23 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 import { execSync } from "child_process";
 
 export default defineConfig({  
   base: '/threejs-terrain/', // Replace 'my-threejs-app' with your GitHub repository name
-  define: {
-    __BUILD_COMMIT__ :  JSON.stringify(execSync("git rev-parse --short HEAD").toString().trim()),
-    __BUILD_DATE__: JSON.stringify(new Date().toISOString()),
-    __BUILD_NUMBER__: JSON.stringify(execSync("git rev-list --count HEAD").toString().trim()),
-  },
+    build: {
+      rollupOptions: {
+        input: {
+          main: resolve(__dirname, 'index.html'),
+          demo1: resolve(__dirname, 'demo1.html'),
+          demo2: resolve(__dirname, 'demo2.html'),
+          demo3: resolve(__dirname, 'demo3.html'),
+        },
+      },    
+      target: 'es2022'
+    },  
+    define: {
+      __BUILD_COMMIT__ :  JSON.stringify(execSync("git rev-parse --short HEAD").toString().trim()),
+      __BUILD_DATE__: JSON.stringify(new Date().toISOString()),
+      __BUILD_NUMBER__: JSON.stringify(execSync("git rev-list --count HEAD").toString().trim()),
+    },
 });
